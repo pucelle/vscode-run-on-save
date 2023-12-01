829afe22cd6616fb06dc971156308ecd05e8b81b
Jack Whelpton <jack.whelpton@fabric.inc>
2023-12-01 14:31:01 -0800

    Use URIs in unit tests

--- test/src/extension.test.ts
+++ test/src/extension.test.ts
@@ -1,7 +1,8 @@
 import * as assert from 'assert'
+import * as path from 'path'
+import {Uri} from 'vscode'
 import {RawCommand, CommandProcessor} from '../../out/command-processor'
 import {FileIgnoreChecker} from '../../out/file-ignore-checker'
-import * as path from 'path'
 
 
 suite("Extension Tests", () => {
@@ -17,7 +18,7 @@ suite("Extension Tests", () => {
 		}])
 
 		test('will compile scss file in backend', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('folderName/fileName.scss')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('folderName/fileName.scss'))
 			assert.deepStrictEqual(commands, [{
 				'runIn': 'backend',
 				'command': 'node-sass folderName/fileName.scss folderName/fileName.css',
@@ -28,12 +29,12 @@ suite("Extension Tests", () => {
 		})
 
 		test('will exclude scss file that file name starts with "_"', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('folderName/_fileName.scss')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('folderName/_fileName.scss'))
 			assert.deepStrictEqual(commands, [])
 		})
 		
 		test('will escape white spaces', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('folderName/fileName 1.scss')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('folderName/fileName 1.scss'))
 			assert.deepStrictEqual(
 				commands[0].command,
 				'node-sass "folderName/fileName 1.scss" "folderName/fileName 1.css"'
@@ -54,7 +55,7 @@ suite("Extension Tests", () => {
 		}])
 
 		test('will compile scss file in backend', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('folderName/fileName.scss')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('folderName/fileName.scss'))
 			assert.deepStrictEqual(commands, [{
 				'runIn': 'backend',
 				'command': 'node-sass folderName/fileName.scss folderName/fileName.css',
@@ -78,7 +79,7 @@ suite("Extension Tests", () => {
 		}])
 
 		test('will compile scss file in backend', function () {
-			let commands = manager.prepareCommandsForFileBeforeSaving('folderName/fileName.scss')
+			let commands = manager.prepareCommandsForFileBeforeSaving(Uri.file('folderName/fileName.scss'))
 			assert.deepStrictEqual(commands, [{
 				'runIn': 'backend',
 				'command': 'node-sass folderName/fileName.scss folderName/fileName.css',
@@ -102,7 +103,7 @@ suite("Extension Tests", () => {
 		}])
 
 		test('will escape paths starts with "\\\\"', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('\\\\folderName\\fileName 1.scss')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('\\\\folderName\\fileName 1.scss'))
 			assert.deepStrictEqual(
 				commands[0].command,
 				'node-sass "\\\\folderName\\fileName 1.scss" "\\\\folderName\\fileName 1.css"'
@@ -121,7 +122,7 @@ suite("Extension Tests", () => {
 		}])
 
 		test('will compile scss file in terminal', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('folderName/fileName.scss')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('folderName/fileName.scss'))
 			assert.deepStrictEqual(commands, [{
 				'runIn': 'terminal',
 				'command': 'node-sass folderName/fileName.scss folderName/fileName.css',
@@ -140,7 +141,7 @@ suite("Extension Tests", () => {
 		}])
 
 		test('will compile it right', function () {
-			let commands = manager.prepareCommandsForFileAfterSaving('test.drawio')
+			let commands = manager.prepareCommandsForFileAfterSaving(Uri.file('test.drawio'))
 			assert.deepStrictEqual(commands, [{
 				'runIn': 'backend',
 				'command': 'draw.io --crop --export -f pdf "test.drawio"',
